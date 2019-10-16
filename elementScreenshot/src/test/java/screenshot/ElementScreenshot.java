25320837a95fc32586fefcf160220cbdff5a89ab
Avinash <avinash.shetty53@yahoo.com>
2019-10-17 00:31:38 +0530

    Adding Rectangle class methods and comments

--- elementScreenshot/src/test/java/screenshot/ElementScreenshot.java
+++ elementScreenshot/src/test/java/screenshot/ElementScreenshot.java
@@ -8,6 +8,7 @@ import java.util.concurrent.TimeUnit;
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
+import org.openqa.selenium.Rectangle;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -30,7 +31,7 @@ public class ElementScreenshot {
 				"webdriver.gecko.driver",
 				path+"\\Drivers\\geckodriver.exe");
 		driver = new FirefoxDriver();
-		driver.manage().window().maximize();
+		driver.manage().window().fullscreen();
 		driver.get("https://www.meripustak.com/");
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
@@ -43,6 +44,12 @@ public class ElementScreenshot {
 		WebElement loginButton = driver.findElement(By
 				.xpath("//a[contains(text(),'Login')]"));
 		wait.until(ExpectedConditions.visibilityOf(loginButton));
+		
+		//Using Rectangle Class and its corresponding methods
+		Rectangle rec = loginButton.getRect();
+		System.out.println("Height: "+rec.getHeight()+","+"Width: "+rec.getWidth()+","+"Xcord: " +rec.getX()+","+"Ycord: "+rec.getY()+","+"Dimension: "+rec.getDimension());
+		
+		//Element Screenshot using getScreenshotAs method
 		GetElementScreenshot.getElementScreenshot(loginButton,methodName);
 		loginButton.click();
 		
